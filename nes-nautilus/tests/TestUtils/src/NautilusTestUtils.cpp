fb399a10c5cadbba726c52a2bdbc0209eb4fd1a3
Nils L. Schubert <nilslpschubert@gmail.com>
2025-12-12 17:08:30 +0100

    fix(Nautilus): Update llvm-v21 and nautilus for vcpkg
    Signed-off-by: Nils L. Schubert <nilslpschubert@gmail.com>

--- nes-nautilus/tests/TestUtils/src/NautilusTestUtils.cpp
+++ nes-nautilus/tests/TestUtils/src/NautilusTestUtils.cpp
@@ -51,6 +51,7 @@
 #include <nautilus/val.hpp>
 #include <nautilus/val_ptr.hpp>
 #include <std/sstream.h>
+#include <std/string.h>
 #include <ErrorHandling.hpp>
 
 namespace NES::TestUtils
@@ -266,7 +267,9 @@ std::string NautilusTestUtils::compareRecords(
         }
         ss << valueRight << ") ";
     }
-    return printErrorMessage ? ss.str().c_str().value : "";
+    const auto strPtr = nautilus::details::RawValueResolver<const char*>::getRawValue(ss.str().c_str());
+    const auto strSize = nautilus::details::RawValueResolver<uint64_t>::getRawValue(ss.str().size());
+    return printErrorMessage ? std::string{strPtr, strSize} : "";
 }
 
 }
