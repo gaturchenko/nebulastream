1de48e0faf3146ea42a627ce2da3d7e695ba8272
Grigorii Turchenko <turchenko.gregory@gmail.com>
2026-06-09 14:40:36 +0200

    feat(scripts): add Flink object detection query

---
+++ scripts/benchmarking/e2e/run_flink.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ./scripts/benchmarking/e2e/baselines/flink-openvino-baseline
+
+#./scripts/run-local.sh osu/pretrained/nanodet/nanodet.xml CPU 1000 1 0 8
+./scripts/run-csv-object-detection.sh \
+  osu/pretrained/nanodet/nanodet.xml \
+  osu_rgb/otcbvs_osu_rgb.csv \
+  CPU \
+  1 \
+  16 \
+  4 \
+  0 \
+  false \
+  320 \
+  320 \
+  false
